c9a5a39d206b508d609e1ab5e6b9b75b33ea4591
Edward Vella <hello@dwardu.com>
2020-01-05 11:14:29 +0000

    Cleaning up old packages

--- setup.py
+++ setup.py
@@ -5,18 +5,23 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-    name='photo_sorter',
-    version='1.0.0',
+    name='photo-org',
+    version=open("version", "r").read(),
     author='Edward Vella',
     author_email='hello@dwardu.com',
     url='https://github.com/dwardu89/photo_sorter',
     description='A tool that sorts photos in a dated folder structure.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages = setuptools.find_packages(),
-    install_requires = [
-        'Pillow==7.0.0'
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
     ],
+    packages = setuptools.find_packages(),
+    install_requires=[
+            "exifread", "Pillow", "colorama"
+        ],
+    include_package_data=True,
+    zip_safe=False,
     entry_points = {
         'console_scripts': [
             'photo_sorter = photo_sorter.__main__:main'
